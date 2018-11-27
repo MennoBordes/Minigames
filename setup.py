9ac5292490df8c23bf782cccc76a0fa0fb0ce2ba
Skillerde6de <0948157@hr.nl>
2018-11-27 20:58:30 +0100

    Remove CX_Freeze

--- setup.py
+++
@@ -1,8 +0,0 @@
-import cx_Freeze
-executables = [cx_Freeze.Executable("Intro.py")]
-
-cx_Freeze.setup(
-    name="Return to Eden",
-    options={"build_exe": {"packages": ["pygame"]}},
-    executables=executables
-)
